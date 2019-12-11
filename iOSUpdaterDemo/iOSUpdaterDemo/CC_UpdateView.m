cad24dcd1f7d19c7ad233e047369fecc9873fc6a
马海平 <haiping@cecelive.com>
2019-12-11 20:45:05 +0800

    add demo

---
+++ iOSUpdaterDemo/iOSUpdaterDemo/CC_UpdateView.m
@@ -0,0 +1,21 @@
+//
+//  CC_UpdateView.m
+//  iOSUpdaterDemo
+//
+//  Created by Apple on 2019/12/11.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+#import "CC_UpdateView.h"
+
+@implementation CC_UpdateView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
